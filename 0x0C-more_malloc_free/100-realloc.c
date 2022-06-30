e10d1f942b69ffe006f5994a6ca3ed28d7bc17e3
thedavidinyang <davidinyang01@gmail.com>
2022-06-30 08:35:09 -0700

    another commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,49 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * _realloc - function that reallocates a memory block using malloc and free
- * @ptr: pointer to the old array
- * @old_size: size of the memory space to allocate in bytes
- * @new_size: size of type
- * Return: void pointer
+ * _realloc - reallocates a memory block.
+ * @ptr: pointer to the memory previously allocated.
+ * @old_size: size, in bytes, of the allocated space of ptr.
+ * @new_size: new size, in bytes, of the new memory block.
+ *
+ * Return: ptr.
+ * if new_size == old_size, returns ptr without changes.
+ * if malloc fails, returns NULL.
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *newptr;
-
-	if (ptr == NULL)
-	{
-		newptr = malloc(new_size);
-		if (newptr == NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		free(ptr);
-		return (newptr);
-	}
-	if (old_size == 0)
-		return (NULL);
-	if (new_size == old_size)
-		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size > old_size)
-	{
-		newptr = malloc(new_size);
-		if (newptr == NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		_memcpy(newptr, ptr, old_size);
-		free(ptr);
-	}
-	return (newptr);
+
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+
+	if (new_size == old_size)
+		return (ptr);
+
+	free(ptr);
+	ptr = malloc(new_size);
+
+	return (ptr);
 }
